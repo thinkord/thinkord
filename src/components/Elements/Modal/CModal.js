cf70805f80557eab650e2d205de5e85c7c73b068
shih-chuan <shihchuan.4all@gmail.com>
2020-12-25 11:27:25 +0800

    feat: new collection and block style

---
+++ src/components/Elements/Modal/CModal.js
@@ -0,0 +1,117 @@
+/* eslint-disable react/prop-types */
+/* eslint-disable @typescript-eslint/explicit-module-boundary-types */
+import React, { useState, useContext } from "react";
+import Modal from "react-bootstrap/Modal";
+import { StoreUpdateContext } from "../../../context";
+
+export default function CModal({ id, folderId, title, modalShow, modalFunc, handleModalToggle }) {
+    const [newTitle, setNewTitle] = useState(title);
+    const { addCollection, addFolder, updateCollectionTitle, deleteCollection } = useContext(StoreUpdateContext);
+
+    const handleTitleChange = (event) => {
+        setNewTitle(event.target.value);
+    };
+
+    const handleNoteRename = (newTitle, noteId) => {
+        updateCollectionTitle(newTitle, noteId);
+        handleModalToggle();
+    };
+    const handleNoteDelete = (noteId) => {
+        deleteCollection(noteId);
+        handleModalToggle();
+    };
+    const handleFolderCreate = () => {
+        addFolder(newTitle);
+        handleModalToggle();
+    };
+    const handleNoteCreate = (title, folderId) => {
+        addCollection(title, folderId);
+        handleModalToggle();
+    };
+
+    var modalDialog;
+    switch (modalFunc) {
+        case "rename":
+            modalDialog = (
+                <Modal show={modalShow} onHide={handleModalToggle} centered>
+                    <Modal.Header className="modal_header">
+                        <Modal.Title>Rename</Modal.Title>
+                    </Modal.Header>
+                    <Modal.Body>
+                        <input onChange={handleTitleChange} type="text" defaultValue={title} />
+                    </Modal.Body>
+                    <Modal.Footer className="modal_footer">
+                        <i
+                            className="modal_icon fas fa-check-circle"
+                            onClick={() => {
+                                handleNoteRename(newTitle, id);
+                            }}
+                        ></i>
+                        <i className="modal_icon fas fa-times-circle" onClick={handleModalToggle}></i>
+                    </Modal.Footer>
+                </Modal>
+            );
+            break;
+        case "addFolder":
+            modalDialog = (
+                <Modal show={modalShow} onHide={handleModalToggle} centered>
+                    <Modal.Header className="modal_header">
+                        <Modal.Title>Name Your New Folder</Modal.Title>
+                    </Modal.Header>
+                    <Modal.Body>
+                        <input type="text" placeholder="untitled" onChange={handleTitleChange} />
+                    </Modal.Body>
+                    <Modal.Footer className="modal_footer">
+                        <i
+                            className="modal_icon fas fa-check-circle"
+                            onClick={() => {
+                                handleFolderCreate();
+                            }}
+                        ></i>
+                        <i className="modal_icon fas fa-times-circle" onClick={handleModalToggle}></i>
+                    </Modal.Footer>
+                </Modal>
+            );
+            break;
+        case "addCollection":
+            modalDialog = (
+                <Modal show={modalShow} onHide={handleModalToggle} centered>
+                    <Modal.Header className="modal_header">
+                        <Modal.Title>Name Your New Note</Modal.Title>
+                    </Modal.Header>
+                    <Modal.Body>
+                        <input type="text" placeholder="untitled" onChange={handleTitleChange} />
+                    </Modal.Body>
+                    <Modal.Footer className="modal_footer">
+                        <i
+                            className="modal_icon fas fa-check-circle"
+                            onClick={() => {
+                                handleNoteCreate(newTitle, folderId);
+                            }}
+                        ></i>
+                        <i className="modal_icon fas fa-times-circle" onClick={handleModalToggle}></i>
+                    </Modal.Footer>
+                </Modal>
+            );
+            break;
+        default:
+            modalDialog = (
+                <Modal show={modalShow} onHide={handleModalToggle} centered>
+                    <Modal.Header className="modal_header">
+                        <Modal.Title>Delete</Modal.Title>
+                    </Modal.Header>
+                    <Modal.Body>Do you really want to delete file $`{title}` ?</Modal.Body>
+                    <Modal.Footer className="modal_footer">
+                        <i
+                            className="modal_icon fas fa-check-circle"
+                            onClick={() => {
+                                handleNoteDelete(id);
+                            }}
+                        ></i>
+                        <i className="modal_icon fas fa-times-circle" onClick={handleModalToggle}></i>
+                    </Modal.Footer>
+                </Modal>
+            );
+    }
+    return <>{modalDialog}</>;
+}
--- src/pages/Work.js
+++ src/pages/Work.js
@@ -1,47 +1,31 @@
 /* eslint-disable @typescript-eslint/explicit-module-boundary-types */
 /* eslint-disable react/prop-types */
 import React, { useContext } from "react";
-import { makeStyles } from "@material-ui/core/styles";
 import { StoreUpdateContext } from "../context";
 import Collection from "../components/Collection/Collection";
 import { Button } from "@material-ui/core";
 import { Link } from "react-router-dom";
 import appRuntime from "../appRuntime";
+import InsertToolBar from "../components/InsertToolBar/InsertToolBar";
 
-const useStyle = makeStyles(() => ({
-    root: {
-        display: "flex",
-        minHeight: "100vh",
-        //background: "grey",
-    },
-}));
 const Work = ({ match }) => {
     const { getCollection, saveCollection } = useContext(StoreUpdateContext);
 
-    const classes = useStyle();
+    //const [collectionId] = useState(match.params.id);
+
+    // const collection = getCollection(collectionId);
     const collection = getCollection(match.params.id);
 
     return (
-        <>
-            {collection !== null ? (
-                <div className={classes.root}>
-                    <Link
-                        className="App-link"
-                        to="/"
-                        onClick={() => {
-                            appRuntime.send("window-channel", "close");
-                        }}
-                    >
-                        Link to Home
-                    </Link>
-
-                    <Collection collection={collection} key={match.params.id} />
-                    <Button onClick={() => saveCollection()}>Save File</Button>
-                </div>
-            ) : (
-                <h1>There is no collection</h1>
-            )}
-        </>
+        <div>
+            {/* <Link className="App-link" to="/">
+                Link to Home
+            </Link>
+            <Collection collection={collection} key={collectionId} /> */}
+            <Collection collection={collection} key={match.params.id} />
+            <button onClick={() => saveCollection()}>Save File</button>
+            <InsertToolBar />
+        </div>
     );
 };
 
